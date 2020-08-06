d759329c2ecb4ccd1491e6bd39459f4c9c1066b1
Noah Henry <nhenryproductions@gmail.com>
2020-08-05 21:42:13 -0500

    added ability to copy values from previous period when creating a new period

--- my-app/src/app/periods/periods.component.ts
+++ my-app/src/app/periods/periods.component.ts
@@ -31,32 +31,51 @@ export class PeriodsComponent implements OnInit {
   }
 
   createNewPeriod() {
-    console.log("Creating a new Period...");
     let newPeriodIdValue = this.periods.length + 1
+    let lastPeriod = this.periods.length -1
 
-    let newPeriod = PeriodMaker.create({ 
-                                  id: newPeriodIdValue,
-                                  isNew: true,
-                                  periodParameters: { 
-                                    initialBalance: 0,
-                                    interestRate: 0.00,
-                                    contributionAmount: 0,
-                                    contributionFrequency: FREQUENCIES.monthly,
-                                    periodLength: 0,
-                                    periodClassification: PERIODCLASSIFICATIONS.year_s,
-                                    compoundFrequency: FREQUENCIES.monthly
-                                  },
-                                  periodResult: {
-                                    id: newPeriodIdValue,
-                                    initialBalance: 0,
-                                    rollovers: [],
-                                    totalContributionsForPeriod: 0,
-                                    totalInterestEarnedForPeriod: 0,
-                                    endOfPeriodBalance: 0
-                                  }
-                                })
-
-    console.log("newPeriod:", newPeriod)
+    // create a new period or copy over values from previous period if it exists
+    let newPeriod = !this.periods ? PeriodMaker.create({ 
+                                      id: newPeriodIdValue,
+                                      isNew: true,
+                                      periodParameters: { 
+                                        initialBalance: 0,
+                                        interestRate: 0.00,
+                                        contributionAmount: 0,
+                                        contributionFrequency: FREQUENCIES.monthly,
+                                        periodLength: 0,
+                                        periodClassification: PERIODCLASSIFICATIONS.year_s,
+                                        compoundFrequency: FREQUENCIES.monthly
+                                      },
+                                      periodResult: {
+                                        id: newPeriodIdValue,
+                                        initialBalance: 0,
+                                        rollovers: [],
+                                        totalContributionsForPeriod: 0,
+                                        totalInterestEarnedForPeriod: 0,
+                                        endOfPeriodBalance: 0
+                                      }
+                                    }) : PeriodMaker.create({ 
+                                      id: newPeriodIdValue,
+                                      isNew: true,
+                                      periodParameters: { 
+                                        initialBalance: this.periods[lastPeriod].periodResult.endOfPeriodBalance,
+                                        interestRate: this.periods[lastPeriod].periodParameters.interestRate,
+                                        contributionAmount: this.periods[lastPeriod].periodParameters.contributionAmount,
+                                        contributionFrequency: this.periods[lastPeriod].periodParameters.contributionFrequency,
+                                        periodLength: this.periods[lastPeriod].periodParameters.periodLength,
+                                        periodClassification: this.periods[lastPeriod].periodParameters.periodClassification,
+                                        compoundFrequency: this.periods[lastPeriod].periodParameters.compoundFrequency
+                                      },
+                                      periodResult: {
+                                        id: newPeriodIdValue,
+                                        initialBalance: 0,
+                                        rollovers: [],
+                                        totalContributionsForPeriod: 0,
+                                        totalInterestEarnedForPeriod: 0,
+                                        endOfPeriodBalance: 0
+                                      }
+                                    })
 
     // HACK: needed trigger re-render of 'periods' arr/object in data-results component
     let tempArr = [].concat(this.periods); // make a copy of the 'periods' array
